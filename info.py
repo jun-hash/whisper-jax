bf4a6dcda96408ea65da759d4f1686869d6249df
junseon <wnstjsy@korea.ac.kr>
2025-02-24 16:02:25 +0900

    update

---
+++ info.py
@@ -0,0 +1,6 @@
+import jax
+import numpy as np
+
+print(jax.devices())        # TPU 장치 목록 출력
+print(jax.__version__)      # JAX 버전 출력 (0.4.x 확인)
+print(np.__version__)       # NumPy 버전 출력 (1.23.x 확인)
